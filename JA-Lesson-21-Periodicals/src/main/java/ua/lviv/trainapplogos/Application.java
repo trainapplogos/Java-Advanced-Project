111b65dafa089045d7c3e981cf3d59fcb0186abe
trainapp <75678105+trainapplogos@users.noreply.github.com>
2021-08-29 14:06:29 +0300

    Updating Application class and pom.xml

--- JA-Lesson-21-Periodicals/src/main/java/ua/lviv/trainapplogos/Application.java
+++ JA-Lesson-21-Periodicals/src/main/java/ua/lviv/trainapplogos/Application.java
@@ -2,8 +2,10 @@ package ua.lviv.trainapplogos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"ua.lviv.trainapplogos"})
 public class Application {
 
 	public static void main(String[] args) {
